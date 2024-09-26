4ee4c016491564a61b341fc63d654bcfe3d72cd2
omarkosp <123820488+omarkosp@users.noreply.github.com>
2024-09-26 20:06:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const { Storage } = require('@google-cloud/storage');
 
-// Replace with your actual project ID
+// Create a client
 const storage = new Storage({
     projectId: 'kanakopoulos', // Your actual Project ID
-    keyFilename: 'key.json', // The path to your JSON key file, assuming it's in the root
+    keyFilename: 'key.json',    // The path to your JSON key file
 });
 
 // Function to upload a file
 async function uploadFile(fileName) {
+    console.log(`Attempting to upload: ${fileName}`); // Log the file being uploaded
     try {
         await storage.bucket('photoskanakopoulos').upload(fileName);
         console.log(`${fileName} uploaded to photoskanakopoulos.`);
     } catch (error) {
-        console.error('Error uploading file:', error);
+        console.error('Error uploading file:', error); // Log the error if upload fails
     }
 }
 
